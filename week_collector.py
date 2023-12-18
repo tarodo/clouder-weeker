c35d0d76b26199defe5c73992073851e146044fd
tarodo <rsvolozhanin@gmail.com>
2023-12-19 00:36:04 +0400

    fix: code refactoring

--- week_collector.py
+++ week_collector.py
@@ -7,10 +7,10 @@ import requests
 from environs import Env
 
 import week_dates
-from spotify_collector import get_track_by_isrc, create_sp, create_playlist, add_tracks
+from spotify_collector import (add_tracks, create_playlist, create_sp,
+                               get_track_by_isrc)
 
 DATA_DIR = "data"
-FULL_WEEK_FILE = "full_week.json"
 WEEK_RAW_DIR = "week_raw"
 TRACKS_RAW_DIR = "tracks_raw"
 TRACKS_SPOTIFY_DIR = "tracks_spotify"
@@ -24,6 +24,7 @@ RELEASES_URL = f"{BP_BASE_URL}/v4/catalog/releases"
 
 BP_STYLES = {
     1: "DNB",
+    90: "TECHNO",
 }
 
 
@@ -73,54 +74,9 @@ def handle_week(year: int, week_number: int, style_id: int, bp_token: str):
     collect_week(start, end, style_id, bp_token, data_path)
 
 
-def collect_essential_week(raw_dir_path: Path):
-    releases = []
-    for week_page in raw_dir_path.iterdir():
-        with open(week_page, "r") as f:
-            data = json.load(f)
-
-        for item in data["results"]:
-            artists = item["artists"]
-            release_artists = []
-            for artist in artists:
-                release_artist = {
-                    "id": artist["id"],
-                    "name": artist["name"],
-                    "url": artist["url"],
-                }
-                release_artists.append(release_artist)
-
-            remixers = item["remixers"]
-            release_remixers = []
-            for remixer in remixers:
-                release_remixer = {
-                    "id": remixer["id"],
-                    "name": remixer["name"],
-                    "url": remixer["url"],
-                }
-                release_remixers.append(release_remixer)
-            release = {
-                "artists": release_artists,
-                "remixers": release_remixers,
-                "catalog_number": item["catalog_number"],
-                "new_release_date": item["new_release_date"],
-                "publish_date": item["publish_date"],
-                "updated": item["updated"],
-                "upc": item["upc"],
-                "label_name": item["label"]["name"],
-                "label_id": item["label"]["id"],
-                "desc": item["desc"],
-                "release_id": item["id"],
-                "release_name": item["name"],
-                "release_url": item["url"],
-                "track_count": item["track_count"],
-            }
-            releases.append(release)
-    return releases
-
-
 def handle_one_release(release: dict, raw_tracks_path: Path, bp_token: str):
-    url = f"{RELEASES_URL}/{release.get('release_id')}/tracks/"
+    release_id = release.get("id")
+    url = f"{RELEASES_URL}/{release_id}/tracks/"
     params = {
         "page": 1,
         "per_page": 100,
@@ -129,37 +85,8 @@ def handle_one_release(release: dict, raw_tracks_path: Path, bp_token: str):
     r = requests.get(url, params=params, headers=headers)
     r.raise_for_status()
     tracks = r.json()
-    with open(f"{raw_tracks_path}/{release.get('release_id')}.json", "w") as f:
+    with open(f"{raw_tracks_path}/{release_id}.json", "w") as f:
         json.dump(tracks, f, indent=4)
-
-
-def handle_week_tracks(full_week_path: Path, raw_tracks_path: Path, bp_token: str):
-    with open(full_week_path, "r") as f:
-        releases = json.load(f)
-
-    for release in releases:
-        handle_one_release(release, raw_tracks_path, bp_token)
-
-
-def collect_tracks_info(raw_tracks_path: Path):
-    tracks = []
-    for release_file in raw_tracks_path.iterdir():
-        with open(release_file, "r") as f:
-            data = json.load(f)
-        for item in data["results"]:
-            track = {
-                "artists": item["artists"],
-                "id": item["id"],
-                "isrc": item["isrc"],
-                "mix_name": item["mix_name"],
-                "name": item["name"],
-                "new_release_date": item["new_release_date"],
-                "publish_date": item["publish_date"],
-            }
-            if item["genre"]["id"] != 1:
-                continue
-            tracks.append(track)
-    return tracks
 
 
 def collect_tracks(year: int, week_num: int, style_id: int, bp_token: str):
@@ -168,17 +95,15 @@ def collect_tracks(year: int, week_num: int, style_id: int, bp_token: str):
 
     releases_path = Path(DATA_DIR) / style_name / f"{year}" / week_num
     raw_dir_path = releases_path / WEEK_RAW_DIR
-    full_week_path = releases_path / FULL_WEEK_FILE
 
     raw_tracks_path = releases_path / TRACKS_RAW_DIR
     os.makedirs(raw_tracks_path, exist_ok=True)
 
-    releases = collect_essential_week(raw_dir_path)
-    with open(full_week_path, "w") as f:
-        json.dump(releases, f, indent=4)
-
-    handle_week_tracks(full_week_path, raw_tracks_path, bp_token)
-    tracks = collect_tracks_info(raw_tracks_path)
+    for week_page in raw_dir_path.iterdir():
+        with open(week_page, "r") as f:
+            page_data = json.load(f)
+        for item in page_data["results"]:
+            handle_one_release(item, raw_tracks_path, bp_token)
 
 
 def collect_spotify_releases(year: int, week_num: int, style_id: int):
@@ -205,7 +130,7 @@ def collect_spotify_releases(year: int, week_num: int, style_id: int):
                 release_sp_tracks.append(sp_track)
                 tracks_count += 1
             else:
-                not_found.append(f"Track : {track['name']} : not found with isrc : {track['isrc']}")
+                not_found.append(track)
         with open(sp_release_path, "w") as f:
             json.dump(release_sp_tracks, f, indent=4)
     if not_found:
@@ -228,7 +153,10 @@ def create_spotify_playlist(year: int, week_num: int, style_id: int):
     logger.info(f"Playlist : {playlist_name} : created : {playlist_url}")
     tracks_ids = []
     for release_file in spotify_tracks_path.iterdir():
+        if release_file.name == NOT_FOUND_FILE:
+            continue
         release_sp_tracks = json.load(open(release_file, "r"))
+
         tracks_ids.extend([track["id"] for track in release_sp_tracks])
     logger.info(f"Tracks count ready to load : {len(tracks_ids)}")
     add_tracks(sp, playlist_id, tracks_ids)
@@ -238,9 +166,9 @@ if __name__ == "__main__":
     env = Env()
     env.read_env()
     bp_token = env.str("BP_TOKEN")
-    week_num = 11
+    week_num = 10
     year = 2023
-    style_id = 1
+    style_id = 90
     handle_week(year, week_num, style_id, bp_token)
     collect_tracks(year, week_num, style_id, bp_token)
     collect_spotify_releases(year, week_num, style_id)
