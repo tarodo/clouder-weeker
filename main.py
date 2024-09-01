d0469bd90504d2955ad924fce5c37f89ce29b71e
tarodo <rsvolozhanin@gmail.com>
2024-09-01 13:58:37 +0400

    fix: correct main typing

--- main.py
+++ main.py
@@ -12,18 +12,20 @@ setup_logging()
 logger = logging.getLogger("main")
 
 
-def bp_release_processing(release_attr: ReleaseMeta, bp_url: str, bp_token: str):
-    logger.info(f"Collect bp releases :: {release_attr.clouder_week} :: Start")
+def bp_release_processing(release_attr: ReleaseMeta, bp_url: str, bp_token: str) -> list[int]:
+    """Collect and save BP releases, return a list of release IDs."""
+    logger.info(f"Collect BP releases :: {release_attr.clouder_week} :: Start")
     for releases in collect_releases(release_attr, bp_url, bp_token):
         for release in releases:
             release["clouder_week"] = release_attr.clouder_week
         save_data_mongo_by_id(releases, "bp_releases")
-    logger.info(f"Collect bp releases :: {release_attr.clouder_week} :: Done")
+    logger.info(f"Collect BP releases :: {release_attr.clouder_week} :: Done")
     return collect_bp_releases(release_attr.week_start.isoformat(), release_attr.week_end.isoformat())
 
 
-def bp_tracks_processing(release_attr: ReleaseMeta, release_ids: list, bp_url: str, bp_token: str) -> list:
-    logger.info(f"Collect bp tracks :: {release_attr.clouder_week} :: Start")
+def bp_tracks_processing(release_attr: ReleaseMeta, release_ids: list, bp_url: str, bp_token: str) -> list[dict]:
+    """Collect and save tracks for each BP release."""
+    logger.info(f"Collect BP tracks :: {release_attr.clouder_week} :: Start")
     for idx, release_id in enumerate(release_ids):
         logger.info(f"Handle release from bp : {idx + 1}/{len(release_ids)} :: Start")
         for tracks in handle_one_release(release_id, bp_url, bp_token):
@@ -31,16 +33,18 @@ def bp_tracks_processing(release_attr: ReleaseMeta, release_ids: list, bp_url: s
                 track["clouder_week"] = release_attr.clouder_week
             save_data_mongo_by_id(tracks, "bp_tracks")
         logger.info(f"Handle release from bp : {idx + 1}/{len(release_ids)} :: Done")
-    logger.info(f"Collect bp tracks :: {release_attr.clouder_week} :: Done")
+    logger.info(f"Collect BP tracks :: {release_attr.clouder_week} :: Done")
     return collect_releases_tracks(release_ids)
 
 
-def sp_tracks_processing(release_attr: ReleaseMeta, bp_tracks: list):
+def sp_tracks_processing(release_attr: ReleaseMeta, bp_tracks: list) -> None:
+    """Collect and save SP tracks based on BP tracks."""
     clouder_week = release_attr.clouder_week
     logger.info(f"Collect spotify tracks :: {clouder_week} :: BP {len(bp_tracks)} :: Start")
     not_found = []
-    one_sp_pack = []
+    sp_tracks = []
     found_cnt = 0
+
     for bp_track in bp_tracks:
         sp_track = get_track_by_isrc(bp_track["isrc"])
         if not sp_track:
@@ -50,26 +54,33 @@ def sp_tracks_processing(release_attr: ReleaseMeta, bp_tracks: list):
         sp_track['album'].pop('available_markets', None)
         sp_track["bp_id"] = bp_track["id"]
         sp_track["clouder_week"] = clouder_week
-        one_sp_pack.append(sp_track)
+        sp_tracks.append(sp_track)
+
         found_cnt += 1
-        if len(one_sp_pack) == 100:
-            save_data_mongo_by_id(one_sp_pack, "sp_tracks")
-            logger.info(f"Collect spotify tracks :: {clouder_week} :: BP {len(bp_tracks)} :: SP {found_cnt}")
-            one_sp_pack.clear()
-    if one_sp_pack:
-        save_data_mongo_by_id(one_sp_pack, "sp_tracks")
-    logger.info(f"Collect spotify tracks :: {clouder_week} :: BP {len(bp_tracks)} :: SP {found_cnt} :: Start")
+        if len(sp_tracks) >= 100:
+            save_data_mongo_by_id(sp_tracks, "sp_tracks")
+            logger.info(f"Collect spotify tracks :: {clouder_week} :: Saved {found_cnt} of {len(bp_tracks)} tracks")
+            sp_tracks.clear()
+
+    if sp_tracks:
+        save_data_mongo_by_id(sp_tracks, "sp_tracks")
+        logger.info(f"Collect spotify tracks :: {clouder_week} :: Saved {found_cnt} of {len(bp_tracks)} tracks")
+
     if not_found:
         not_found_week = [{"id": clouder_week, "not_found": not_found},]
         save_data_mongo_by_id(not_found_week, "not_found_sp_tracks")
-        logger.warning(f"Collect spotify tracks :: {clouder_week} :: Not Found {len(not_found_week)} :: Start")
+        logger.warning(f"Spotify tracks not found :: {clouder_week} :: Count {len(not_found)}")
+
+    logger.info(f"Collect spotify tracks :: {clouder_week} :: BP {len(bp_tracks)} :: SP {found_cnt} :: Done")
 
 
 if __name__ == "__main__":
     env = Env()
     env.read_env()
+
     bp_url = env.str("BP_URL")
     bp_token = env.str("BP_TOKEN")
+
     release_attr = ReleaseMeta(week=34, year=2023, style_id=1)
 
     release_ids = bp_release_processing(release_attr, bp_url, bp_token)
